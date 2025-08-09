727a1051bfd0e5246d3a6ae843730b62abe4685c
22510007 <shreya.sharma@walchandsangli.ac.in>
2025-08-09 12:36:35 +0530

    Update 02_01_vec_scalar_add.c

--- practical-2/02_01_vec_scalar_add.c
+++ practical-2/02_01_vec_scalar_add.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <omp.h>
 
 int main() {
     int n = 10;
+    int threads;
     float scalar = 2.5;
-    float vec[10], result[10];
+    float *vec, *res;
+
+    printf("Enter vector size: ");
+    scanf("%d", &n);
+
+    printf("Enter number of threads: ");
+    scanf("%d", &threads);
+
+    vec = (float*)malloc(n * sizeof(float));
+    res = (float*)malloc(n * sizeof(float));
+
+    srand(time(NULL)); // used to make different random numbers every time we run the program
 
     for (int i = 0; i < n; i++) {
-        vec[i] = i * 1.0;
+        vec[i] = rand() % 100;
     }
 
     printf("Original Vector: ");
@@ -15,22 +29,24 @@ int main() {
         printf("%.1f ", vec[i]);
     }
     printf("\nScalar to add: %.1f\n", scalar);
-
     
+    omp_set_num_threads(threads);
     double start = omp_get_wtime();
 
     #pragma omp parallel for
     for (int i = 0; i < n; i++) {
-        result[i] = vec[i] + scalar;
+        res[i] = vec[i] + scalar;
     }
 
     double end = omp_get_wtime();
 
     printf("Result Vector: ");
     for (int i = 0; i < n; i++) {
-        printf("%.1f ", result[i]);
+        printf("%.1f ", res[i]);
     }
-    printf("\nTime taken: %.6f seconds\n", end - start);
+    printf("\nTime taken with %d threads for %d data size: %.6f seconds\n", threads, n, end - start);
 
+    free(vec);
+    free(res);
     return 0;
 }
