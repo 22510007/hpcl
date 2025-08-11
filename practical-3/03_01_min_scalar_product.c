97876366c30716522f3a8e5928f0951bc897ea65
22510007 <shreya.sharma@walchandsangli.ac.in>
2025-08-11 19:29:29 +0530

    Update 03_01_min_scalar_product.c

--- practical-3/03_01_min_scalar_product.c
+++ practical-3/03_01_min_scalar_product.c
@@ -1,43 +1,89 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <omp.h>
+#include <time.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Quick sort
+void quickSortAsc(int arr[], int low, int high) {
+    if (low < high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(&arr[i], &arr[j]);
+            }
+        }
+        swap(&arr[i + 1], &arr[high]);
+        int pi = i + 1;
+
+        quickSortAsc(arr, low, pi - 1);
+        quickSortAsc(arr, pi + 1, high);
+    }
+}
+
+void quickSortDesc(int arr[], int low, int high) {
+    if (low < high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] > pivot) {
+                i++;
+                swap(&arr[i], &arr[j]);
+            }
+        }
+        swap(&arr[i + 1], &arr[high]);
+        int pi = i + 1;
+
+        quickSortDesc(arr, low, pi - 1);
+        quickSortDesc(arr, pi + 1, high);
+    }
+}
 
 int main() {
-    int n = 5;
-    int vec1[] = {1, 3, -5, 2, -1};
-    int vec2[] = {-2, 4, 1, -3, 0};
+    int n, threads;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
 
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (vec1[i] > vec1[j]) {
-                int temp = vec1[i];
-                vec1[i] = vec1[j];
-                vec1[j] = temp;
-            }
-        }
-    }
+    printf("Enter number of threads: ");
+    scanf("%d", &threads);
 
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (vec2[i] < vec2[j]) {
-                int temp = vec2[i];
-                vec2[i] = vec2[j];
-                vec2[j] = temp;
-            }
-        }
-    }
+    int A[n], B[n];
 
-    int result = 0;
-    double start = omp_get_wtime();
+    srand(time(NULL));
 
-    #pragma omp parallel for reduction(+:result)
     for (int i = 0; i < n; i++) {
-        result += vec1[i] * vec2[i];
+        A[i] = rand() % 100;
+        B[i] = rand() % 100;
     }
 
-    double end = omp_get_wtime();
+    quickSortAsc(A, 0, n - 1);
+    quickSortDesc(B, 0, n - 1);
 
-    printf("Minimum Scalar Product = %d\n", result);
-    printf("Time taken: %f seconds\n", end - start);
+    long min_dot = 0;
+    long min_dot_reduction = 0;
+    omp_set_num_threads(threads);
+
+    #pragma omp parallel for
+    for (int i = 0; i < n; i++) {
+        min_dot += (long)A[i] * B[i]; 
+    }
+
+    #pragma omp parallel for reduction(+:min_dot_reduction)
+    for (int i = 0; i < n; i++) {
+        min_dot_reduction += (long)A[i] * B[i];
+    }
+
+    printf("\nResult without reduction (may be WRONG): %ld\n", min_dot);
+    printf("Result with reduction: %ld\n", min_dot_reduction);
 
     return 0;
 }
